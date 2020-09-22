50b944b6c001a8a250d47d2d98a778bd0bccde1c
UpsidePotential <mikeafuchs@gmail.com>
2020-09-21 20:38:35 -0500

    add helo world

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import * as captureWebsite from 'capture-website';
+import fs from 'fs';
 
 require('dotenv').config();
 
@@ -13,5 +15,33 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!');
+    res.send('wecome to zombocom!')
+});
+
+app.get('/spy', (req, res) =>  {
+    captureWebsite.file('https://finviz.com/map.ashx', 'screenshot.png', {
+        width: 1200,
+        height: 2000,
+        element: 'canvas.chart',
+        launchOptions: {
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+          ],
+        },
+      }).then( ()=> {
+        const img = fs.readFileSync('screenshot.png')
+
+        res.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': img.length
+        });
+        res.end(img); 
+        fs.unlink('screenshot.png', () => {});
+      })
+  
+      
+      
+
+
 });
