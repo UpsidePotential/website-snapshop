79e0e3cedec8961e379e909de88a08b02688fb31
UpsidePotential <mikeafuchs@gmail.com>
2020-09-21 21:44:24 -0500

    get more

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import * as captureWebsite from 'capture-website';
 import fs from 'fs';
 
 require('dotenv').config();
 
 const app = express();
+app.use(cors())
 let port = process.env.PORT;
 if (port === undefined || port === '') {
   port = 8000 as any;
@@ -22,6 +24,7 @@ app.get('/spy', (req, res) =>  {
     captureWebsite.file('https://finviz.com/map.ashx', 'screenshot.png', {
         width: 1200,
         height: 2000,
+        timeout: 90000,
         element: 'canvas.chart',
         launchOptions: {
           args: [
@@ -30,7 +33,7 @@ app.get('/spy', (req, res) =>  {
           ],
         },
       }).then( ()=> {
-        const img = fs.readFileSync('screenshot.png')
+        const img = fs.readFileSync('screenshot.png', { encoding: 'base64'})
 
         res.writeHead(200, {
             'Content-Type': 'image/png',
@@ -39,9 +42,28 @@ app.get('/spy', (req, res) =>  {
         res.end(img); 
         fs.unlink('screenshot.png', () => {});
       })
-  
-      
-      
+});
 
+app.get('/etf', (req, res) =>  {
+    captureWebsite.file('https://finviz.com/map.ashx?t=etf', 'screenshot.png', {
+        width: 1200,
+        height: 2000,
+        timeout: 90000,
+        element: 'canvas.chart',
+        launchOptions: {
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+          ],
+        },
+      }).then( ()=> {
+        const img = fs.readFileSync('screenshot.png', { encoding: 'base64'})
 
+        res.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': img.length
+        });
+        res.end(img); 
+        fs.unlink('screenshot.png', () => {});
+      })
 });
