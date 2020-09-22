09ce7e0b61d4f80b44964b979780e2de7215289a
UpsidePotential <mikeafuchs@gmail.com>
2020-09-21 19:15:47 -0500

    shell

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+require('dotenv').config();
+
+const app = express();
+let port = process.env.PORT;
+if (port === undefined || port === '') {
+  port = 8000 as any;
+}
+
+app.listen(port, () => {
+  console.info(`App Listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
